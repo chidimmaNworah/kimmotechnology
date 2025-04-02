a4ef20fc45be30e10939c5f501a39261bb9e17b8
chidimmaNworah <chidimmanworah12@gmail.com>
2025-04-02 09:49:37 +0100

    adding monetag ad script

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,18 @@ import "@/styles/base.scss";
 import { useEffect } from "react";
 import TagManager from "react-gtm-module";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        id="monetag-ad-script"
+        strategy="lazyOnLoad"
+        dangerouslySetInnerHTML={{
+          html: `(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',9161384,document.createElement('script'))`,
+        }}
+      />
       <Component {...pageProps} />
       <GoogleTagManager gtmId={`${process.env.NEXT_PUBLIC_GTM_ID}`} />
       <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GA_ID}`} />
