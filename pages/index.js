b776789aff5a1b8f76b2b19bad13c6443671f596
chidimmaNworah <chidimmanworah12@gmail.com>
2025-04-15 16:03:11 +0100

    adding npmrc for legacy-peer-deps build boolean1

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { Navbar } from "@/components";
 import Countdown from "@/components/CountDown";
 import Newsletter from "@/components/Newsletter";
